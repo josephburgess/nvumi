438dba3dbe39f324ecd85339a5cb8965053b14bb
josephburgess <josephburgess@gmail.com>
2025-02-13 11:35:07 -0300

    Add command to open scratch with nvumi FT

--- lua/nvumi/main.lua
+++ lua/nvumi/main.lua
@@ -1,2 +1,32 @@
 local M = {}
+local function run_numi_line()
+end
+
+function M.open()
+	local opts = {
+		name = "Nvumi",
+		ft = "nvumi", -- custom ft to avoid conflicts
+		win_by_ft = {
+			nvumi = {
+				keys = {
+					["source"] = {
+						"<CR>",
+						function(self)
+							run_numi_line()
+						end,
+						mode = { "n", "x" },
+					},
+				},
+			},
+		},
+	}
+	require("snacks.scratch").open(opts)
+end
+
+function M.setup()
+	vim.api.nvim_create_user_command("Nvumi", function()
+		M.open()
+	end, {})
+end
+
 return M
