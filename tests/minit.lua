9ba59199108e8b9a598831cbfc55567f9e8cb47c
josephburgess <josephburgess@gmail.com>
2025-02-14 14:21:37 -0300

    Tidy

--- tests/minit.lua
+++ tests/minit.lua
@@ -3,15 +3,9 @@
 vim.env.LAZY_STDPATH = ".tests"
 load(vim.fn.system("curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua"))()
 
+-- mocked to avoid errs
 package.preload["nvim-web-devicons"] = function()
-  return {
-    set_icon = function(_) end,
-  }
+  return { set_icon = function(_) end }
 end
 
--- Setup lazy.nvim
-require("lazy.minit").setup({
-  spec = {
-    { dir = vim.uv.cwd() },
-  },
-})
+require("lazy.minit").setup({ spec = { { dir = vim.uv.cwd() } } })
