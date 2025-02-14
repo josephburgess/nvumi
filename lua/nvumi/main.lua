359ad2245914e0fd5ecf169d36aacc68ff58868a
josephburgess <josephburgess@gmail.com>
2025-02-14 11:31:21 -0300

    abstract rendering

--- lua/nvumi/main.lua
+++ lua/nvumi/main.lua
@@ -5,11 +5,25 @@ local fn = vim.fn
 local schedule = vim.schedule
 local ns = api.nvim_create_namespace("nvumi_inline")
 
+local function render_inline(buf, line_index, result)
+  api.nvim_buf_set_extmark(buf, ns, line_index, 0, {
+    virt_text = { { " = " .. result, "Comment" } },
+    virt_text_pos = "eol",
+  })
+end
+
+local function render_newline(buf, line_index, result)
+  local virt_lines = {}
+  for _, txt in ipairs(vim.split(result, "\n")) do
+    table.insert(virt_lines, { { " " .. txt, "Comment" } })
+  end
+  api.nvim_buf_set_extmark(buf, ns, line_index, 0, { virt_lines = virt_lines })
+end
+
 local function run_numi_on_buffer()
   local config = require("nvumi.config").options
   local buf = api.nvim_get_current_buf()
   local lines = api.nvim_buf_get_lines(buf, 0, -1, false)
-
   api.nvim_buf_clear_namespace(buf, ns, 0, -1)
 
   for i, line in ipairs(lines) do
@@ -21,16 +35,9 @@ local function run_numi_on_buffer()
             schedule(function()
               local result = table.concat(data, " ")
               if config.virtual_text == "inline" then
-                api.nvim_buf_set_extmark(buf, ns, i - 1, 0, {
-                  virt_text = { { " = " .. result, "Comment" } },
-                  virt_text_pos = "eol",
-                })
+                render_inline(buf, i - 1, result)
               else
-                local virt_lines = {}
-                for _, txt in ipairs(vim.split(result, "\n")) do
-                  table.insert(virt_lines, { { " " .. txt, "Comment" } })
-                end
-                api.nvim_buf_set_extmark(buf, ns, i - 1, 0, { virt_lines = virt_lines })
+                render_newline(buf, i - 1, result)
               end
             end)
           end
