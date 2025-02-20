5bf7790ce3e83ce692d5704c1a0de7b25f9ee64c
Joe Burgess <josephburgess@gmail.com>
2025-02-19 21:13:44 -0300

    chore: refactor (#21)

--- lua/nvumi/units.lua
+++ lua/nvumi/units.lua
@@ -2,40 +2,36 @@ local config = require("nvumi.config")
 local M = {}
 
 function M.format_date(result)
-  local format = config.options.date_format
   local year, month, day = result:match("(%d%d%d%d)-(%d%d)-(%d%d)")
 
   if not year then
     return result
   end
 
-  if format == "iso" then
-    return result
-  end
+  local format = config.options.date_format
 
-  if format == "us" then
-    return ("%s/%s/%s"):format(month, day, year)
-  elseif format == "uk" then
-    return ("%s/%s/%s"):format(day, month, year)
-  elseif format == "long" then
-    local months = {
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    }
-    return ("%s %s, %s"):format(months[tonumber(month)], day, year)
-  end
+  local months = {
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  }
 
-  return result
+  local formats = {
+    us = ("%s/%s/%s"):format(month, day, year),
+    uk = ("%s/%s/%s"):format(day, month, year),
+    long = ("%s %s, %s"):format(months[tonumber(month)], day, year),
+  }
+
+  return formats[format] or result
 end
 
 return M
