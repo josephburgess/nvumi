47beaea2795ab0a4982bf1f94142949feef0a397
Joe Burgess <josephburgess@gmail.com>
2025-02-20 18:28:20 -0300

    chores: better error notification; readme tweaks; add autocmd group (#24)
    Docs
    * `README.md`: Tweaks, typo correction, added to roadmap ideas
    * `nvumi.txt`: small typo in config example in helptxt doc
    
    Code:
    * `lua/nvumi/autocmds.lua`: Added a command group for autocommands
    * `lua/nvumi/runner.lua`: Enhanced error notif when `numi-cli` is not installed with directions to :help Nvumi

--- lua/nvumi/autocmds.lua
+++ lua/nvumi/autocmds.lua
@@ -3,9 +3,12 @@ local debounce = require("nvumi.debounce").debounce
 
 local M = {}
 
+local cmd_group = vim.api.nvim_create_augroup("NvumiAutocmds", { clear = true })
+
 function M.setup()
   -- setup comments/syntax highlighting
   vim.api.nvim_create_autocmd("FileType", {
+    group = cmd_group,
     pattern = "nvumi",
     callback = function()
       vim.bo.commentstring = "-- %s"
@@ -19,6 +22,7 @@ function M.setup()
 
   -- evaluate whole buffer on exit insert
   vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+    group = cmd_group,
     pattern = "*.nvumi",
     callback = function()
       actions.run_on_buffer()
@@ -28,6 +32,7 @@ function M.setup()
   -- live evaluations
   local debounced_rol = debounce(300, actions.run_on_line)
   vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+    group = cmd_group,
     pattern = "*.nvumi",
     callback = function()
       debounced_rol()
