41df52667cb793ac900548ae6f97d7bd874c6987
josephburgess <josephburgess@gmail.com>
2026-04-05 18:58:45 +0100

    big cleanup to autocmds

--- lua/nvumi/autocmds.lua
+++ lua/nvumi/autocmds.lua
@@ -6,36 +6,39 @@ local M = {}
 local cmd_group = vim.api.nvim_create_augroup("NvumiAutocmds", { clear = true })
 
 function M.setup()
-  -- setup comments/syntax highlighting
   vim.api.nvim_create_autocmd("FileType", {
     group = cmd_group,
     pattern = "nvumi",
-    callback = function()
-      vim.bo.commentstring = "-- %s"
-      vim.cmd("set syntax=nvumi")
+    callback = function(ev)
+      vim.bo[ev.buf].commentstring = "-- %s"
+      vim.cmd("setlocal syntax=nvumi")
       vim.cmd([[
         syntax clear Comment
         syntax match Comment /^\s*--.*/
       ]])
-    end,
-  })
 
-  -- evaluate whole buffer on exit insert
-  vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-    group = cmd_group,
-    pattern = "*.nvumi",
-    callback = function()
-      actions.run_on_buffer()
-    end,
-  })
+      local buf_group = vim.api.nvim_create_augroup("NvumiBuffer_" .. ev.buf, { clear = true })
 
-  -- live evaluations
-  local debounced_rol = debounce(300, actions.run_on_line)
-  vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
-    group = cmd_group,
-    pattern = "*.nvumi",
-    callback = function()
-      debounced_rol()
+      vim.api.nvim_create_autocmd("InsertLeave", {
+        group = buf_group,
+        buffer = ev.buf,
+        callback = actions.run_on_buffer,
+      })
+
+      local debounced_rob = debounce(300, actions.run_on_buffer)
+      local debounced_rol = debounce(300, actions.run_on_line)
+
+      vim.api.nvim_create_autocmd("TextChanged", {
+        group = buf_group,
+        buffer = ev.buf,
+        callback = debounced_rob,
+      })
+
+      vim.api.nvim_create_autocmd("TextChangedI", {
+        group = buf_group,
+        buffer = ev.buf,
+        callback = debounced_rol,
+      })
     end,
   })
 end
